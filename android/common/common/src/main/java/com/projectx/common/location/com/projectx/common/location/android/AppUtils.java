434a633a70259fe69943578b0329b6e150a6d70e
MIchael Rock <mjames.rock@gmail.com>
2017-07-09 20:08:30 -0700

    Initial commit of common

---
+++ android/common/common/src/main/java/com/projectx/common/location/com/projectx/common/location/android/AppUtils.java
@@ -0,0 +1,26 @@
+package com.projectx.common.location.com.projectx.common.location.android;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.os.Build;
+
+public class AppUtils {
+
+    /**
+     * @return true If device has Android Marshmallow or above version
+     */
+    public static boolean hasM() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.M;
+    }
+
+    public static boolean isServiceRunning(Context context, Class serviceClass) {
+        ActivityManager manager =
+                (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (serviceClass.getName().equals(service.service.getClassName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
